677207db727795da41f30b169e27e4d276dddb55
Gavin Cernek <gcernek@gmail.com>
2023-09-06 21:56:38 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('node:http');
+const fs = require('node:fs');
+
+const hostname = 'localhost';
+const port = 5000;
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+	if (req.url === '/') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                    <script type="module" src="./scripts/index.js"></script>
+                </head>
+                <body>
+                    <h1>Hello World!</h1>
+                    <div id="root">
+                        <button type="button" id="btn">
+                            Click Me
+                        </button>
+                    </div>
+                </body>
+            </html>
+        `);
+	} else if (req.url === '/scripts/index.js') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/javascript');
+		res.end(fs.readFileSync('./scripts/index.js'));
+	} else if (req.url === '/html') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`<p>Swapped with SSR'd content!</p>`);
+	}
+});
+
+server.listen(port, hostname);
---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const root = document.getElementById('root');
+const btn = document.getElementById('btn');
+
+class Test {
+	root;
+	trigger;
+	fetch;
+
+	constructor(root, trigger, fetch) {
+		this.root = root;
+		this.trigger = trigger;
+		this.fetch = fetch;
+	}
+
+	async update() {
+		try {
+			const res = await this.fetch();
+			this.root.innerHTML = res;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
+
+const test = new Test(root, btn, async () => {
+	try {
+		const res = await fetch('/html');
+
+		const reader = res.body.getReader();
+		const stream = await reader.read();
+
+		const html = new TextDecoder().decode(stream.value);
+		return html;
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+btn.addEventListener('click', async () => test.update());
